902a93d8f24a537fa781d21f180f4ecd017ae7ca
Milton Isaya <miltonisaiah@gmail.com>
2023-05-11 12:24:47 +0300

    css for table added

---
+++ src/components/MuiComponents/Table/TableFooter/index.tsx
@@ -0,0 +1,17 @@
+
+const TableRow = () :any =>{
+    return (
+        <tbody>
+        <tr>
+            <td>January</td>
+            <td>$100</td>
+        </tr>
+        <tr>
+            <td>February</td>
+            <td>$80</td>
+        </tr>
+        </tbody>
+    );
+}
+
+export default TableRow;
---
+++ src/components/MuiComponents/Table/TableRow/index.tsx
@@ -0,0 +1,13 @@
+
+const TableHead = () :any =>{
+    return (
+        <thead>
+        <tr>
+            <th>Month</th>
+            <th>Savings</th>
+        </tr>
+        </thead>
+    );
+}
+
+export default TableHead;
