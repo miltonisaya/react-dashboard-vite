3bc17db55be15ca2fc941d36eed826cc1ec3326a
Milton <miltonisaiah@gmail.com>
2023-06-07 11:18:43 +0300

    text field props extended

---
+++ src/components/UiComponents/BasicTextField/index.tsx
@@ -0,0 +1,12 @@
+import TextField, {TextFieldProps} from '@mui/material/TextField';
+
+export type CustomTextFieldProps = {
+    label: string;
+    id: string;
+} & TextFieldProps
+
+export default function BasicTextField(props: CustomTextFieldProps) {
+    return (
+            <TextField {...props} />
+    );
+}
