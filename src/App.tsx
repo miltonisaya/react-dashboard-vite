c168ea490f23ce287361027a69ef64cc40011997
Milton <miltonisaiah@gmail.com>
2023-06-09 11:51:57 +0300

    dialog added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
-import './App.css'
+import './App.css';
 import BasicButton from "./components/UiComponents/BasicButton";
-import BasicTextField from "./components/UiComponents/BasicTextField";
-
+import FormDialog from "./components/UiComponents/Dialog";
+import {useState} from "react";
 
 function App() {
-    const handleClick =()=>{
-        console.log("Clicked!!")
-    }
+    const [isOpen, setOpen] = useState(false);
+    const openDialog = () => {
+        setOpen(true);
+    };
 
-    const handleKeyUp = ()=>{
-        console.log("Key up!!")
-    }
+    const closeDialog = () => {
+        setOpen(false);
+    };
+
     return (
-            <>
-                <BasicTextField id={'name'} label={'name'} onChange={()=>handleKeyUp()}/>
-                <BasicButton variant={'contained'} onClick={()=>handleClick()}>Click Me!</BasicButton>
-            </>
-    )
+        <>
+            <FormDialog
+                title={'Test Dialog'}
+                isOpen={isOpen}
+                onClose={closeDialog}
+            />
+            <BasicButton variant={'contained'} onClick={() => openDialog()}>
+                Open Dialog
+            </BasicButton>
+        </>
+    );
 }
 
-export default App
+export default App;
